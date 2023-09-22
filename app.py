d70e3f366f584ce3d4db3517a7c6d3f46b23d7af
phionahingabire <phionahingabire40@gmail.com>
2023-09-22 14:54:36 +0300

    flask.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-    return "hello, woprld!"
+    return "hello, world!"
 if __name__ == (__name__):
     app.run()
